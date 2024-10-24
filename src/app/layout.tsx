464391dc331e19a22c941918dc297e1fecfce922
matteopilotto <matteo.pilotto@protonmail.com>
2024-10-24 12:05:26 +0200

    initialize messages with Convex API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ConvexClient } from "convex/browser";
+import { ConvexClientProvider } from "./convex-client-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import { useQuery, useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+// interface Message {
+//   sender: string;
+//   content: string;
+// }
+
 export default function Home() {
-  return <div>Hello, World!</div>;
+  // const [messages, setMessages] = useState<Message[]>([
+  //   { sender: "Alice", content: "Hello, world!" },
+  //   { sender: "Bob", content: "Hi, Alice!" },
+  // ]);
+
+  const messages = useQuery(api.functions.message.list);
+  const createMessage = useMutation(api.functions.message.create);
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // setMessages([...messages, { sender: "Alice", content: input }]);
+    createMessage({ sender: "Alice", content: input });
+    setInput("");
+  };
+
+  return (
+    <div>
+      {messages?.map((message, index) => (
+        <div key={index}>
+          <strong>{message.sender}</strong>: {message.content}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
